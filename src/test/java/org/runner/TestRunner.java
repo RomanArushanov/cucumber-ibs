aa67df4c9355fd2aaf82e10cba3c10e54f6b27bc
RomanArushanov <134087292+RomanArushanov@users.noreply.github.com>
2023-09-04 21:59:29 +0400

    Fixed project

--- src/test/java/org/runner/TestRunner.java
+++ src/test/java/org/runner/TestRunner.java
@@ -7,9 +7,8 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "src/test/java/org/steps"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.steps"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
 public class TestRunner {
